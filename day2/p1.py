5a8193da8fbec65cf53ce7a96eb08b42346de9a5
Oleksii Tokar <oleksiito@wix.com>
2021-12-01 09:43:19 +0200

    day 2 prep

---
+++ day2/p1.py
@@ -0,0 +1,6 @@
+file1 = open('input1.txt', 'r')
+lines = list(map(int,map(str.strip, file1.readlines())))
+
+result = None
+
+print( result )
---
+++ day2/p2.py
@@ -0,0 +1,6 @@
+input = open('input2.txt', 'r')
+lines = list(map(int,map(str.strip, input.readlines())))
+
+result = None
+
+print( result )
