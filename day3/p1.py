f5e6543f571fd190b529689a3d1ce4e08f3effd8
Oleksii Tokar <oleksiito@wix.com>
2021-12-03 10:50:57 +0200

    day3 p1 refactored

--- day3/p1.py
+++ day3/p1.py
@@ -2,24 +2,21 @@ input = open('input1.txt', 'r')
 lines = list(map(str.strip, input.readlines()))
 
 
-result = None
 total = len(lines)
-gamma = [0]*len(lines[0])
-
-for line in lines:
-    for i in range(len(line)):
-        gamma[i] += int(line[i])
 
 result = ""
-result_r = ""
-for g in gamma:
-    result += "1" if g > total/2 else "0"
-    result_r += "1" if g < total/2 else "0"
+for i in range(len(lines[0])):
+    ones = 0
+    for line in lines:
+        ones += int(line[i])
+    result += "1" if ones > total/2 else "0"
 
-print( gamma )
+
 print( result )
 
-print( int(result,2) )
-print( int(result_r,2) )
+r1 = int(result,2)
+r2 = 2**len(lines[0]) - 1 - r1
+print( r1 )
+print( r2 )
 
-print(int(result,2) * int(result_r,2))
+print( r1 * r2 )
