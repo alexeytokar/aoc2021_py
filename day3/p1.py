ea8caecd674ea6b6aa5a51db730d93fe2d9444d9
Oleksii Tokar <oleksiito@wix.com>
2021-12-03 10:55:16 +0200

    day3 p1 refactored

--- day3/p1.py
+++ day3/p1.py
@@ -1,22 +1,11 @@
 input = open('input1.txt', 'r')
 lines = list(map(str.strip, input.readlines()))
 
-
 total = len(lines)
 
 result = ""
 for i in range(len(lines[0])):
-    ones = 0
-    for line in lines:
-        ones += int(line[i])
+    ones = sum(int(line[i]) for line in lines)
     result += "1" if ones > total/2 else "0"
 
-
-print( result )
-
-r1 = int(result,2)
-r2 = 2**len(lines[0]) - 1 - r1
-print( r1 )
-print( r2 )
-
-print( r1 * r2 )
+print( int(result,2) * (2**len(lines[0]) - 1 - int(result,2)) )
