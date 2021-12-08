49314f2e52cb188312664d0c57595d48fd150d8f
Oleksii Tokar <oleksiito@wix.com>
2021-12-08 08:24:54 +0200

    a bit better

--- day8/p2.py
+++ day8/p2.py
@@ -11,29 +11,19 @@ def cnt(o,num):
 
     return len(list(set(l).intersection(n))) == len(num)
 
-print(cnt("bcdefg","bcdef"))
-
 ssssum = 0
 for line in lines:
     p = line.strip().split(" | ")
     right = p[1].split( " " )
     left = p[0].split( " " )
 
-    lefts = []
-    for l in left:
-        lefts.append( ''.join(sorted(l)) )
-
     nums = [""]*10
     ov = []
-    for li in lefts:
-        if len(li) == 2:
-            nums[1] = li
-        elif len(li) == 3:
-            nums[7] = li
-        elif len(li) == 4:
-            nums[4] = li
-        elif len(li) == 7:
-            nums[8] = li
+    size2idx = {2:1,3:7,4:4,7:8}
+    for li in left:
+        li = ''.join(sorted(li))
+        if len(li) in size2idx:
+            nums[size2idx[len(li)]] = li
         else:
             ov.append(li)
 
@@ -48,18 +38,12 @@ for line in lines:
     
     for o in ov:
         if len(o) == 6:
-            print(o)
             if cnt(o,nums[3]):
-                print(9)
                 nums[9] = o
             elif cnt(o,nums[5]):
-                print(6)
                 nums[6] = o
             else:
-                print(0)
                 nums[0] = o
-            print(nums)
-            print('--')
 
     maps = {}
     for n in range(len(nums)):
@@ -70,7 +54,6 @@ for line in lines:
     for r in right:
         s = ''.join(sorted(r))
         tt = tt + str(maps[s])
-    print( int(tt) )
     ssssum = ssssum + int(tt)
 
 
