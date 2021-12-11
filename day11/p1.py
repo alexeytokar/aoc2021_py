fae3e825138354d58dd1409d60a5e5e9285d43ed
Oleksii Tokar <oleksiito@wix.com>
2021-12-11 09:35:00 +0200

    day 11 brute force

---
+++ day11/p1.py
@@ -0,0 +1,53 @@
+# 9.04
+input = open('input1.txt', 'r')
+
+def mat( matrix ):
+    for row in matrix:
+        print(row)
+
+rows = 10
+cols = 10
+matrix = [[0 for i in range(cols)] for j in range(rows)]
+
+lines = input.readlines()
+row = 0
+for line in lines:
+    matrix[row] = list(map(int,list(line.strip())))
+    row = row + 1
+
+mat( matrix )
+
+
+count = 0
+
+def flash(matrix, r, c):
+    global count
+    count += 1
+    for i in range(r - 1, r + 2):
+        for j in range(c - 1, c + 2):
+            if i == r and j == c: 
+                continue
+
+            if 0 <= i < len(matrix) and 0 <= j < len(matrix[i]):
+                matrix[i][j] += 1
+
+                if matrix[i][j] == 10:
+                    flash(matrix, i, j)
+                    matrix[i][j] += 1
+
+
+for step in range(100):
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            matrix[r][c] += 1
+
+            if matrix[r][c] == 10:
+                flash(matrix, r, c)
+                matrix[r][c] += 1
+
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            if matrix[r][c] > 9:
+                matrix[r][c] = 0
+
+print(count)
